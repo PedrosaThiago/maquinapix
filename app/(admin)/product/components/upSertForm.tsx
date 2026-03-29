b52cc9f90a9dd5e71c63e36d326dab377f278b5f
PedrosaThiago <130430096+PedrosaThiago@users.noreply.github.com>
2026-03-29 11:52:41 -0300

    Refactor UpSertForm component and validation schema
    Refactor form validation schema and update form handling. Improve input handling for amount and quantity fields.

--- app/(admin)/product/components/upSertForm.tsx
+++ app/(admin)/product/components/upSertForm.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import React, { useEffect } from 'react'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -8,7 +7,7 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog"
-import { z } from 'zod'
+import z from 'zod'
 import { Button } from '@/components/ui/button'
 import { toast } from 'sonner'
 import { createProductType } from '@/app/types/product'
@@ -18,10 +17,15 @@ import { Input } from '@/components/ui/input'
 import { createOrUpdateProduct } from '@/app/actions/upsert-product'
 import { useAction } from 'next-safe-action/hooks'
 
+
 const formSchema = z.object({
-  amount: z.coerce.number({ message: "Campo obrigatório" }),
-  description: z.string().optional(),
-  quantity: z.coerce.number().optional(),
+  amount: z.number({ message: "Campo obrigatório" }),
+  description: z.string().min(0).optional(),
+  quantity: z
+  .coerce
+  .number()
+  .refine((val) => !isNaN(val), { message: "Informe um número válido" })
+  .optional(),
 })
 
 interface Props {
@@ -30,64 +34,57 @@ interface Props {
   onSuccess?: () => void;
 }
 
-const UpSertForm = ({ isOpen, onSuccess, product }: Props) => {
-  const { execute, isPending } = useAction(createOrUpdateProduct);
+const UpSertForm = ({isOpen, onSuccess, product}: Props) => {
+      const { execute, isPending } = useAction(createOrUpdateProduct);
+      const form = useForm<z.infer<typeof formSchema>>({
+      shouldUnregister: true,
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        amount: product?.amount ? product.amount/100 : 0,
+        description: product?.description ?? "",
+        quantity: product?.quantity ?? 0,
+      },
+    });
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    shouldUnregister: true,
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      amount: product?.amount ? product.amount / 100 : 0,
-      description: product?.description ?? "",
-      quantity: product?.quantity ?? 0,
-    },
-  });
-
-  useEffect(() => {
+    useEffect(() => {
     if (isOpen) {
       form.reset({
-        amount: product?.amount ? product.amount / 100 : 0,
+        amount: product?.amount ? product.amount/100 : 0,
         description: product?.description ?? "",
         quantity: product?.quantity ?? 0,
       });
     }
   }, [isOpen, form, product]);
 
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
-    execute({
-      id: product?.id,
-      amount: data.amount * 100,
-      description: data.description,
-      quantity: data.quantity,
-    });
-
-    onSuccess?.();
-    toast.success("Produto salvo com sucesso");
-  }
+    const onSubmit =  (data: z.infer<typeof formSchema>) => {
+        execute({
+          id: product?.id,
+          amount: data.amount*100,
+          description: data.description,
+          quantity: data.quantity
+        });
+        onSuccess?.();
+        toast.success("Produto salvo com sucesso");
+      }
 
   return (
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>
-          {product ? "Editar produto" : "Adicionar produto"}
-        </DialogTitle>
-      </DialogHeader>
-
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-
-          {/* VALOR */}
-          <FormField
-            control={form.control}
-            name="amount"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Valor*</FormLabel>
-                <FormControl>
-                  <NumericFormat
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>{product ? "Editar produto" : "Adicionar produto"}</DialogTitle>
+    </DialogHeader>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Valor*</FormLabel>
+              <FormControl>
+                <NumericFormat
                     value={field.value}
                     onValueChange={(values) => {
-                      field.onChange(values.floatValue ?? 0);
+                      field.onChange(values.floatValue);
                     }}
                     prefix="R$ "
                     decimalScale={2}
@@ -97,57 +94,44 @@ const UpSertForm = ({ isOpen, onSuccess, product }: Props) => {
                     customInput={Input}
                     allowNegative={false}
                   />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Descrição</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-          {/* DESCRIÇÃO */}
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Descrição</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <FormField
+          control={form.control}
+          name="quantity"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Quantidade</FormLabel>
+              <FormControl>
+                <Input type="number" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        
+        <Button className='w-full capitalize text-foreground' type="submit" disabled={isPending}>{isPending ? "Salvando..." : "Salvar"}</Button>
+      </form>
+    </Form>
+  </DialogContent>
 
-          {/* QUANTIDADE */}
-          <FormField
-            control={form.control}
-            name="quantity"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Quantidade</FormLabel>
-                <FormControl>
-                  <Input
-                    type="number"
-                    {...field}
-                    onChange={(e) => field.onChange(Number(e.target.value))}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <Button
-            className='w-full capitalize text-foreground'
-            type="submit"
-            disabled={isPending}
-          >
-            {isPending ? "Salvando..." : "Salvar"}
-          </Button>
-
-        </form>
-      </Form>
-    </DialogContent>
   )
 }
 
